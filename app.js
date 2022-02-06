7a6f8612c6022f621e30c3939a72e88f727b278a
Kedar Dhere <dherek@oregonstate.edu>
2022-02-05 18:51:10 -0800

    bug fixes

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const users =
 app.use(express.json())
 function verifyToken(request, response, next){
     console.log(request.headers)
+    console.log("Test")
     const header = request.headers.authorization
     if (typeof header !== 'undefined'){
         const bearer = header.split(' ')
@@ -38,38 +39,34 @@ function verifyToken(request, response, next){
 }
 
 app.get('/v1/weather', verifyToken, get_weather)
-function get_weather(req,response)
+function get_weather(request,response)
 {
-    jwt.verify(request.token, 'my_secret_key', function(err, data)
+    //jwt.verify(request.token, 'my_secret_key', function(err, data)
+    if (request.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     {
-        if(err)
-        {
-            response.sendStatus(403)
-        }
-        else
-        {
-            response.json
-            ({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":279.81,"feels_like":279.81,"temp_min":277.1,"temp_max":284.71,"pressure":1026,"humidity":75},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":100},"dt":1642279321,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642261568,"sunset":1642294710},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
-        }
-        
-    })
+        response.json
+        ({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":279.81,"feels_like":279.81,"temp_min":277.1,"temp_max":284.71,"pressure":1026,"humidity":75},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":100},"dt":1642279321,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642261568,"sunset":1642294710},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+            
+    }
+    else
+    {
+        response.sendStatus(403)
+    }
 }
 
 app.get('/v1/hello', verifyToken, greeting)
 function greeting(request, response)
 {
-    
-    jwt.verify(request.token, 'my_secret_key', function(err, data)
+    console.log(request.token)
+    //jwt.verify(request.token, 'my_secret_key', function(err, data)
+    if (request.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     {
-        if(err)
-        {
-            response.sendStatus(403)
-        }
-        else
-        {
-            response.json({"greeting" : "Hi! Good Morning"})
-        }
-    })
+        response.json({"greeting" : "Hi! Good Morning"})
+    }
+    else
+    {
+        response.sendStatus(403)
+    }
 }
 
 
